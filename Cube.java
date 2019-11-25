97638224335005bc06f95330291aba3fb16bd5a6
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2019-11-25 12:17:52 +0300

    Куб

---
+++ Cube.java
@@ -0,0 +1,9 @@
+public class Cube {
+    public static void main(String[] args)
+    {
+        Vector a = new Vector(4,5,6);
+        Vector b = new Vector(1,2,3);
+        Vector c = a.Summ(a,b);
+        c.print(c);
+    }
+}
